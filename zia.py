cec0b2cc96587069b236f0c6eb4c20bc7f70eb1b
zeesial <119788945+zeesial@users.noreply.github.com>
2023-01-03 21:32:18 +0500

    Update zia.py

--- zia.py
+++ zia.py
@@ -390,7 +390,6 @@ class crack_main():
                                         brand_name_ = "Infinix"
                                         width_ = "720"
                                         height_ = "1280"
-                                 accessToken = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
                         fbav = f'{random.randint(111,999)}.0.0.{random.randint(11,99)}.{random.randint(111,999)}'
                         fbbv = str(random.randint(111111111,999999999))
                         android_version = device['android_version']
